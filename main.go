a63038201c6dc7ff04433e30711960a0310b1954
copara <oparadivinefavour1@gmail.com>
2026-05-08 14:49:38 +0100

    slice

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 // uppercase,lowercase,bin,hex
-
 func Readfile(filename string) string {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -41,11 +41,92 @@ func main() {
 			previousword := finaldata[i-1]
 			capword := Captializefirst(previousword)
 			result = append(result[:len(result)-1], capword)
+			continue
+		case "(hex)":
+			previousword := finaldata[i-1]
+			hex, err := strconv.ParseInt(previousword, 16, 64)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			result = append(result[:len(result)-1], strconv.FormatInt(hex, 10))
+		case "(up,":
+			if i+1 < len(finaldata) {
+
+				nextWord := finaldata[i+1]
+
+				numpart, rest, found := strings.Cut(nextWord, ")")
+
+				var intnum int
+
+				var err error
+
+				if found {
+
+					intnum, err = strconv.Atoi(numpart)
+
+					if err != nil {
+
+						intnum = 1
+					}
+
+				} else {
+					intnum = 1
+				}
+
+				start := len(result) - intnum
+
+				if start < 0 {
+					start = 0
+
+				}
+				for j := start; j < len(result); j++ {
+					result[j] = strings.ToUpper(result[j])
+				}
+				if rest != "" {
+					result = append(result, rest)
+				}
+				i++
+
+				continue
+			}
+		case "(low,":
+			if i+1 < len(finaldata) {
+				nextWord := finaldata[i+1]
+				numpart, rest, found := strings.Cut(nextWord, ")")
+
+				var intnum int
+				var err error
+				if found {
+					intnum, err = strconv.Atoi(numpart)
+					if err != nil {
+						intnum = 1
+					}
+				} else {
+					intnum = 1
+				}
+				start := len(result) - intnum
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j < len(result); j++ {
+					result[j] = strings.ToLower(result[j])
+				}
+				if rest != "" {
+					result = append(result, rest)
+				}
+				i++
+				continue
+			}
+
 		default:
 			result = append(result, finaldata[i])
 		}
 
 	}
+
+	fmt.Println(strings.Join(result, " "))
 	// for i := 0; i < len(finaldata); i++ {
 	// 	switch finaldata[i] {
 	// 	case "(low)":
@@ -56,6 +137,4 @@ func main() {
 	// 		result = append(result, finaldata[i])
 	// 	}
 	// }
-
-	fmt.Println(result)
 }
